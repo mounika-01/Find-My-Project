0e14a78e0b9b01f6376cadda382064d4d275a826
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-16 09:49:36 +0530

    Update Project.java

--- src/main/java/com/example/findmyproject/model/Project.java
+++ src/main/java/com/example/findmyproject/model/Project.java
@@ -50,7 +50,9 @@ public class Project {
         return budget;
     }
 
-   public void setBudget(double budget) { this.budget = budget; }
+    public void setBudget(double budget) { 
+        this.budget = budget; 
+    }
 
 
     public List<Researcher> getResearchers() {
